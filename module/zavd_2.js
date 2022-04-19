00e9ee1edb3b82b534d412b04cd2c411b4603a08
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2022-04-19 09:46:35 +0300

    Update zavd_2.js

--- module/zavd_2.js
+++ module/zavd_2.js
@@ -1,12 +1,12 @@
 document.getElementById("find-number").onclick = () => {
     let number = document.getElementById("car number").value;
-    if ((number.charAt(0) == 'A' || number.charAt(1) == 'C') || (number.charAt(0) == 'B' || number.charAt(1) == 'K') (number.charAt(0) == 'B' || number.charAt(1) == 'C')){
+    if (number.charAt(0) == 'A' && number.charAt(1) == 'C' || number.charAt(0) == 'B' && number.charAt(1) == 'K' || number.charAt(0) == 'B' && number.charAt(1) == 'C'){
         region = "Західна область";
-    } else if ((number.charAt(0) == 'A' || number.charAt(1) == 'M') || (number.charAt(0) == 'A' || number.charAt(1) == 'A') (number.charAt(0) == 'A' || number.charAt(1) == 'I')){
+    } else if (number.charAt(0) == 'A' && number.charAt(1) == 'M' || number.charAt(0) == 'A' && number.charAt(1) == 'A' || number.charAt(0) == 'A' && number.charAt(1) == 'I'){
         region = "Центральна область";
-    } else if ((number.charAt(0) == 'A' || number.charAt(1) == 'X') || (number.charAt(0) == 'A' || number.charAt(1) == 'H') (number.charAt(0) == 'B' || number.charAt(1) == 'B')){
+    } else if (number.charAt(0) == 'A' && number.charAt(1) == 'X' || number.charAt(0) == 'A' && number.charAt(1) == 'H' || number.charAt(0) == 'B' && number.charAt(1) == 'B'){
         region = "Східна область";
-    } else if ((number.charAt(0) == 'B' || number.charAt(1) == 'H') || (number.charAt(0) == 'B' || number.charAt(1) == 'E') (number.charAt(0) == 'B' || number.charAt(1) == 'T')){
+    } else if (number.charAt(0) == 'B' && number.charAt(1) == 'H' || number.charAt(0) == 'B' && number.charAt(1) == 'E' || number.charAt(0) == 'B' && number.charAt(1) == 'T'){
         region = "Південна область";
     } else{
         alert("Введіть правильний номер")
