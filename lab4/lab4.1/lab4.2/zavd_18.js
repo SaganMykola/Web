a932373287e96158bf1f1aed939aa6cd879ccd8e
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2022-04-24 18:19:24 +0300

    Update zavd_18.js

--- lab4/lab4.1/lab4.2/zavd_18.js
+++ lab4/lab4.1/lab4.2/zavd_18.js
@@ -1,9 +1,9 @@
 let title = prompt("Введіть заголовок: ");
 let image = prompt("Введіть посилання на зображення: ");
 let passing = prompt("Введіть посилання для переходу: ");
-function getLinks(title, image, passing){
+function banner(title, image, passing){
     document.write(`<h1>${title}</h1>`);
     document.write(`<a href='${passing}'><img src='${image}'></img></a>`);
 }
 
-getLinks(title, image, passing)
+banner(title, image, passing)
