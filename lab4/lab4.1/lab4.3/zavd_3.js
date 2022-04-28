74f28660dd12f6ce796e6540bb5c39bbf96caf9b
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2022-04-28 10:57:42 +0300

    Update zavd_3.js

--- lab4/lab4.1/lab4.3/zavd_3.js
+++ lab4/lab4.1/lab4.3/zavd_3.js
@@ -1,4 +1,8 @@
 let input_name = prompt("Введіть ім'я: ", "Юрій");
 let list = ["Юрій", "Юрій", "Микола"];
+let num = 0;
+for (let i = 0; i < list.length; i++)
+    if (list[i].toLowerCase() == input_name.toLowerCase())
+        num++ ;
 
-alert(`Вказане користувачем ім’я зустрічається ${list.filter(el => el.toLowerCase() == input_name.toLowerCase()).length} разів`);
+alert(`Вказане користувачем ім’я зустрічається ${num} разів`);
