eb021e53f3d615aefb853dd244ca55b59eed834d
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2022-05-03 13:29:38 +0300

    Update zavd_4.js

--- lab4/lab4.1/lab4.3/zavd_4.js
+++ lab4/lab4.1/lab4.3/zavd_4.js
@@ -1,25 +1,26 @@
 let list = Array();
-for (let i = 0; i < 5; i++)
+let used = Array();
+
+for (let i = 0; i < 5 ; i++)
     list.push(parseInt(Math.random() * 1000) - 500);
-alert(list);
 
 let prise = 0;
 
 while(true){
-    let n = prompt("Введіть номер елемента або стоп щоб відмовитись від подальшої  ", "Від 0 до 4");
-    prise += list[n];
-    alert(`Сумарний виграш: ${prise}`)
-    list.splice(n, 1, 0);
-    let a = list.reduce(function(x, y){ 
-        return x + y;
-    });
-
-    let m = confirm("Хочете продовжити гру?")
-    if (a == 0 || m == false){
-        break
-    }
-
+    var n = prompt("Введіть номер елемента або 'stop' щоб відмовитись від подальшої гри ", "Від 0 до 4");
+    if (used.length == list.length || n == "stop")
+        break;
     else
-        continue
+    {
+        if (used.includes(n))
+            alert("Ви вже використали цей номер");
+
+        else
+        {
+            used.push(n);
+            prise += list[n];
+            alert(`Сумарний виграш: ${prise}`);
+        }
+    }
 }
 alert(`Ваш виграш: ${prise}`)
